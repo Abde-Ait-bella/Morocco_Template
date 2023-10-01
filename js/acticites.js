f84045a9161439713307c6c017a18e663d309e03
abdessamad <AIT-BELLA.ABDESSAMAD@ofppt-edu.ma>
2023-10-01 17:39:36 +0100

    last commit

--- js/acticites.js
+++ js/acticites.js
@@ -1,16 +1,20 @@
-var card = document.querySelector('.activite-1');
+// var card = document.querySelector('.activite-1');
 
-var isDragging = false;
+// var isDragging = false;
 
-const dragStart =()=>{
-    isDragging = true;
-}
+// const dragStart =()=>{
+//     isDragging = true;
+// }
 
-const moveCard = (e)=>{
-    console.log(e.pageX);
-    if (!isDragging)return;
-    card.scrollLeft = e.pageX;
-}
+// const moveCard = (e)=>{
+//     console.log(e.pageX);
+//     if (!isDragging)return;
+//     card.scrollLeft = e.pageX;
+// }
 
-card.addEventListener('mousemove', moveCard)
-card.addEventListener('mousedown', dragStart)
+// card.addEventListener('mousemove', moveCard)
+// card.addEventListener('mousedown', dragStart)
+$(".slider__ville").on("click", function() {
+    $(".backgrounds img").removeClass("visible");
+    $(".backgrounds img").eq($(this).index()).addClass("visible");
+  })
--- js/navMobile.js
+++ js/navMobile.js
@@ -1,23 +1,3 @@
-// var card = document.querySelector('.activite-1');
-
-// var isDragging = false;
-
-// const dragStart =()=>{
-//     isDragging = true;
-// }
-
-// const moveCard = (e)=>{
-//     console.log(e.pageX);
-//     if (!isDragging)return;
-//     card.scrollLeft = e.pageX;
-// }
-
-// card.addEventListener('mousemove', moveCard)
-// card.addEventListener('mousedown', dragStart)
-$(".slider__ville").on("click", function() {
-    $(".backgrounds img").removeClass("visible");
-    $(".backgrounds img").eq($(this).index()).addClass("visible");
-  })
 const navberger = document.getElementsByClassName("navberger");
 const menumobil = document.getElementsByClassName("menumobil");
 
